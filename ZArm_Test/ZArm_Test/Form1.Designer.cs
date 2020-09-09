4aae4d8a764ffe54662bb99da543a161f327794f
Dustin Dobransky <dd2_inventor_climber@hotmail.com>
2020-09-09 01:48:20 -0700

    Adding ZArm_Test project I found kicking around

---
+++ ZArm_Test/ZArm_Test/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace ZArm_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.debugBox = new System.Windows.Forms.TextBox();
+            this.zUpButton = new System.Windows.Forms.Button();
+            this.zDownButton = new System.Windows.Forms.Button();
+            this.shoulderCcwButton = new System.Windows.Forms.Button();
+            this.shoulderCwButton = new System.Windows.Forms.Button();
+            this.elbowCcwButton = new System.Windows.Forms.Button();
+            this.elbowCwButton = new System.Windows.Forms.Button();
+            this.wristCcwButton = new System.Windows.Forms.Button();
+            this.wristCwButton = new System.Windows.Forms.Button();
+            this.zPosTextbox = new System.Windows.Forms.TextBox();
+            this.shoulderPosTextbox = new System.Windows.Forms.TextBox();
+            this.elbowPosTextbox = new System.Windows.Forms.TextBox();
+            this.wristPosTextbox = new System.Windows.Forms.TextBox();
+            this.deltaTextbox = new System.Windows.Forms.TextBox();
+            this.speedTextbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.idButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(12, 12);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(111, 38);
+            this.buttonConnect.TabIndex = 0;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // debugBox
+            // 
+            this.debugBox.Location = new System.Drawing.Point(12, 174);
+            this.debugBox.Multiline = true;
+            this.debugBox.Name = "debugBox";
+            this.debugBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.debugBox.Size = new System.Drawing.Size(369, 134);
+            this.debugBox.TabIndex = 1;
+            // 
+            // zUpButton
+            // 
+            this.zUpButton.Location = new System.Drawing.Point(12, 72);
+            this.zUpButton.Name = "zUpButton";
+            this.zUpButton.Size = new System.Drawing.Size(75, 23);
+            this.zUpButton.TabIndex = 2;
+            this.zUpButton.Text = "Z Up";
+            this.zUpButton.UseVisualStyleBackColor = true;
+            this.zUpButton.Click += new System.EventHandler(this.zUpButton_Click);
+            // 
+            // zDownButton
+            // 
+            this.zDownButton.Location = new System.Drawing.Point(12, 101);
+            this.zDownButton.Name = "zDownButton";
+            this.zDownButton.Size = new System.Drawing.Size(75, 23);
+            this.zDownButton.TabIndex = 2;
+            this.zDownButton.Text = "Z Down";
+            this.zDownButton.UseVisualStyleBackColor = true;
+            this.zDownButton.Click += new System.EventHandler(this.zDownButton_Click);
+            // 
+            // shoulderCcwButton
+            // 
+            this.shoulderCcwButton.Location = new System.Drawing.Point(93, 72);
+            this.shoulderCcwButton.Name = "shoulderCcwButton";
+            this.shoulderCcwButton.Size = new System.Drawing.Size(99, 23);
+            this.shoulderCcwButton.TabIndex = 2;
+            this.shoulderCcwButton.Text = "Shoulder Left";
+            this.shoulderCcwButton.UseVisualStyleBackColor = true;
+            this.shoulderCcwButton.Click += new System.EventHandler(this.shoulderCcwButton_Click);
+            // 
+            // shoulderCwButton
+            // 
+            this.shoulderCwButton.Location = new System.Drawing.Point(93, 101);
+            this.shoulderCwButton.Name = "shoulderCwButton";
+            this.shoulderCwButton.Size = new System.Drawing.Size(99, 23);
+            this.shoulderCwButton.TabIndex = 2;
+            this.shoulderCwButton.Text = "Shoulder Right";
+            this.shoulderCwButton.UseVisualStyleBackColor = true;
+            this.shoulderCwButton.Click += new System.EventHandler(this.shoulderCwButton_Click);
+            // 
+            // elbowCcwButton
+            // 
+            this.elbowCcwButton.Location = new System.Drawing.Point(225, 72);
+            this.elbowCcwButton.Name = "elbowCcwButton";
+            this.elbowCcwButton.Size = new System.Drawing.Size(75, 23);
+            this.elbowCcwButton.TabIndex = 2;
+            this.elbowCcwButton.Text = "Elbow Left";
+            this.elbowCcwButton.UseVisualStyleBackColor = true;
+            this.elbowCcwButton.Click += new System.EventHandler(this.elbowCcwButton_Click);
+            // 
+            // elbowCwButton
+            // 
+            this.elbowCwButton.Location = new System.Drawing.Point(225, 101);
+            this.elbowCwButton.Name = "elbowCwButton";
+            this.elbowCwButton.Size = new System.Drawing.Size(75, 23);
+            this.elbowCwButton.TabIndex = 2;
+            this.elbowCwButton.Text = "Elbow Right";
+            this.elbowCwButton.UseVisualStyleBackColor = true;
+            this.elbowCwButton.Click += new System.EventHandler(this.elbowCwButton_Click);
+            // 
+            // wristCcwButton
+            // 
+            this.wristCcwButton.Location = new System.Drawing.Point(306, 72);
+            this.wristCcwButton.Name = "wristCcwButton";
+            this.wristCcwButton.Size = new System.Drawing.Size(75, 23);
+            this.wristCcwButton.TabIndex = 2;
+            this.wristCcwButton.Text = "Wrist Left";
+            this.wristCcwButton.UseVisualStyleBackColor = true;
+            this.wristCcwButton.Click += new System.EventHandler(this.wristCcwButton_Click);
+            // 
+            // wristCwButton
+            // 
+            this.wristCwButton.Location = new System.Drawing.Point(306, 101);
+            this.wristCwButton.Name = "wristCwButton";
+            this.wristCwButton.Size = new System.Drawing.Size(75, 23);
+            this.wristCwButton.TabIndex = 2;
+            this.wristCwButton.Text = "Wrist Right";
+            this.wristCwButton.UseVisualStyleBackColor = true;
+            this.wristCwButton.Click += new System.EventHandler(this.wristCwButton_Click);
+            // 
+            // zPosTextbox
+            // 
+            this.zPosTextbox.Location = new System.Drawing.Point(13, 131);
+            this.zPosTextbox.Name = "zPosTextbox";
+            this.zPosTextbox.Size = new System.Drawing.Size(74, 20);
+            this.zPosTextbox.TabIndex = 3;
+            // 
+            // shoulderPosTextbox
+            // 
+            this.shoulderPosTextbox.Location = new System.Drawing.Point(93, 131);
+            this.shoulderPosTextbox.Name = "shoulderPosTextbox";
+            this.shoulderPosTextbox.Size = new System.Drawing.Size(99, 20);
+            this.shoulderPosTextbox.TabIndex = 3;
+            // 
+            // elbowPosTextbox
+            // 
+            this.elbowPosTextbox.Location = new System.Drawing.Point(225, 130);
+            this.elbowPosTextbox.Name = "elbowPosTextbox";
+            this.elbowPosTextbox.Size = new System.Drawing.Size(75, 20);
+            this.elbowPosTextbox.TabIndex = 3;
+            // 
+            // wristPosTextbox
+            // 
+            this.wristPosTextbox.Location = new System.Drawing.Point(306, 130);
+            this.wristPosTextbox.Name = "wristPosTextbox";
+            this.wristPosTextbox.Size = new System.Drawing.Size(75, 20);
+            this.wristPosTextbox.TabIndex = 3;
+            // 
+            // deltaTextbox
+            // 
+            this.deltaTextbox.Location = new System.Drawing.Point(306, 12);
+            this.deltaTextbox.Name = "deltaTextbox";
+            this.deltaTextbox.Size = new System.Drawing.Size(75, 20);
+            this.deltaTextbox.TabIndex = 3;
+            this.deltaTextbox.TextChanged += new System.EventHandler(this.deltaTextbox_TextChanged);
+            // 
+            // speedTextbox
+            // 
+            this.speedTextbox.Location = new System.Drawing.Point(306, 38);
+            this.speedTextbox.Name = "speedTextbox";
+            this.speedTextbox.Size = new System.Drawing.Size(75, 20);
+            this.speedTextbox.TabIndex = 3;
+            this.speedTextbox.TextChanged += new System.EventHandler(this.speedTextbox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(243, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Delta [mm]";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(226, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Speed [mm/2]";
+            // 
+            // idButton
+            // 
+            this.idButton.Location = new System.Drawing.Point(129, 15);
+            this.idButton.Name = "idButton";
+            this.idButton.Size = new System.Drawing.Size(72, 35);
+            this.idButton.TabIndex = 2;
+            this.idButton.Text = "Get Id";
+            this.idButton.UseVisualStyleBackColor = true;
+            this.idButton.Click += new System.EventHandler(this.idButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 320);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.speedTextbox);
+            this.Controls.Add(this.deltaTextbox);
+            this.Controls.Add(this.wristPosTextbox);
+            this.Controls.Add(this.elbowPosTextbox);
+            this.Controls.Add(this.shoulderPosTextbox);
+            this.Controls.Add(this.zPosTextbox);
+            this.Controls.Add(this.wristCwButton);
+            this.Controls.Add(this.elbowCwButton);
+            this.Controls.Add(this.shoulderCwButton);
+            this.Controls.Add(this.wristCcwButton);
+            this.Controls.Add(this.elbowCcwButton);
+            this.Controls.Add(this.shoulderCcwButton);
+            this.Controls.Add(this.zDownButton);
+            this.Controls.Add(this.idButton);
+            this.Controls.Add(this.zUpButton);
+            this.Controls.Add(this.debugBox);
+            this.Controls.Add(this.buttonConnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.TextBox debugBox;
+        private System.Windows.Forms.Button zUpButton;
+        private System.Windows.Forms.Button zDownButton;
+        private System.Windows.Forms.Button shoulderCcwButton;
+        private System.Windows.Forms.Button shoulderCwButton;
+        private System.Windows.Forms.Button elbowCcwButton;
+        private System.Windows.Forms.Button elbowCwButton;
+        private System.Windows.Forms.Button wristCcwButton;
+        private System.Windows.Forms.Button wristCwButton;
+        private System.Windows.Forms.TextBox zPosTextbox;
+        private System.Windows.Forms.TextBox shoulderPosTextbox;
+        private System.Windows.Forms.TextBox elbowPosTextbox;
+        private System.Windows.Forms.TextBox wristPosTextbox;
+        private System.Windows.Forms.TextBox deltaTextbox;
+        private System.Windows.Forms.TextBox speedTextbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button idButton;
+    }
+}
+
---
+++ ZArm_Test/ZArm_Test/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TcpserverExDll;
+using ControlBeanExDll;
+using System.Threading;
+
+namespace ZArm_Test
+{
+    public partial class Form1 : Form
+    {
+        const int verticalReach = 310; // mm
+        const int xDir = 1;
+        const int yDir = 2;
+        const int zDir = 3;
+        const int axis1Shoulder = 1;
+        const int axis2Elbow = 2;
+        const int axis3Z = 3;
+        const int axis4Wrist = 4;
+
+        int zArmId = -1;
+
+        ControlBeanEx robot;
+
+        int delta = 20; // mm
+        int speed = 10; // mm/s
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void idButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Task t = Task.Factory.StartNew(() => TcpserverEx.net_port_initial());
+
+                await t;
+
+                if (t.IsFaulted)
+                {
+                    debugBox.AppendText("No net port initial\n");
+                }
+                if (t.IsCompleted)
+                {
+                    debugBox.AppendText("Net port initial completed\n");
+                }
+                if (t.IsCanceled)
+                {
+                    debugBox.AppendText("Cancelled");
+                }
+                if (t.Exception != null)
+                {
+                    throw t.Exception;
+                }
+            }
+            catch (Exception ex)
+            {
+                debugBox.AppendText(ex.Message);
+            }
+
+            Thread.Sleep(1000);
+            
+            bool found = false;
+            try
+            {
+                for (int i = 0; i < 256; i++)
+                {
+                    if (TcpserverEx.card_number_connect(i) == 1)
+                    {
+                        zArmId = i;
+                        found = true;
+                        debugBox.AppendText(string.Format("Zarm id: {0}\n", i));
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                debugBox.AppendText(ex.Message);
+            }
+
+            if (!found)
+            {
+                debugBox.AppendText("No arm found\n");
+            }
+        }
+
+        private async void buttonConnect_Click(object sender, EventArgs e)
+        {
+            robot = TcpserverEx.get_robot(zArmId);
+            bool isConnected = false;
+            while (!isConnected)
+            {
+                isConnected = await Task.Factory.StartNew(() => robot.is_connected());
+                debugBox.AppendText(string.Format("Attempting to connect to Z-Arm {0}... {1}\n", zArmId, isConnected));
+                await Task.Delay(1000);
+            }
+            
+            debugBox.AppendText(string.Format("IsConnected: {0}\n", robot.is_connected()));
+
+            //debugBox.AppendText($"initial_finish: {robot.initial_finish}, communicate_success: {robot.communicate_success}\n");
+
+            int ret = -1;
+
+            while (ret != 1)
+            {
+                ret = await Task.Factory.StartNew(() => robot.initial(1, 310));
+                debugBox.AppendText($"ret: {ret}\n");
+                await Task.Delay(1000);
+            }
+
+            if (ret != 1)
+            {
+                debugBox.AppendText(string.Format("Could not initialize Z Arm id {0}, return value: {1}\n", zArmId, ret));
+            }
+            else
+            {
+                robot.unlock_position();
+                robot.set_arm_length(200, 200);
+                robot.set_catch_or_release_accuracy(0.5f);
+                debugBox.AppendText(string.Format("Z Arm id: {0} connected!\n", zArmId));                
+            }
+        }
+
+        private void zUpButton_Click(object sender, EventArgs e)
+        {
+            robot.xyz_move(zDir, delta, speed);
+        }
+
+        private void zDownButton_Click(object sender, EventArgs e)
+        {
+            robot.xyz_move(zDir, -delta, speed);
+        }
+
+        private void shoulderCcwButton_Click(object sender, EventArgs e)
+        {
+            robot.single_joint_move(axis1Shoulder, delta, speed);
+        }
+
+        private void shoulderCwButton_Click(object sender, EventArgs e)
+        {
+            robot.single_joint_move(axis1Shoulder, -delta, speed);
+        }
+
+        private void elbowCcwButton_Click(object sender, EventArgs e)
+        {
+            robot.single_joint_move(axis2Elbow, delta, speed);
+        }
+
+        private void elbowCwButton_Click(object sender, EventArgs e)
+        {
+            robot.single_joint_move(axis2Elbow, -delta, speed);
+        }
+
+        private void wristCcwButton_Click(object sender, EventArgs e)
+        {
+            robot.single_joint_move(axis4Wrist, delta, speed);
+        }
+
+        private void wristCwButton_Click(object sender, EventArgs e)
+        {
+            robot.single_joint_move(axis4Wrist, -delta, speed);
+        }
+
+        private void deltaTextbox_TextChanged(object sender, EventArgs e)
+        {
+            this.delta = int.Parse(this.deltaTextbox.Text);
+        }
+
+        private void speedTextbox_TextChanged(object sender, EventArgs e)
+        {
+            this.speed = int.Parse(this.speedTextbox.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            TcpserverEx.close_tcpserver();
+        }
+    }
+}
